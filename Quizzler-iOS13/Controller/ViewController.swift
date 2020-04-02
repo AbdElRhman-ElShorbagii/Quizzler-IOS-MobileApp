c0f7d9127f113e0bf40e67a843cc07f09e8fa139
AbdElRhman-ElShorbagii <abdelrhmanelshorbagii@gmail.com>
2020-04-02 16:42:17 +0200

    adiing MVC and finishing true or false app

---
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var quizBrain = QuizBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUi()
+    }
+
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle!
+        let userGotItRight = quizBrain.checkAnswer(userAnswer)
+        
+        //without adding a condition to it, because it's data type is Bool.
+        if userGotItRight {
+            sender.backgroundColor = UIColor.green
+        }
+        else{
+            sender.backgroundColor = UIColor.red
+        }
+        
+        quizBrain.nextQuestion()
+
+        Timer.scheduledTimer(timeInterval: 0.2,target: self,selector: #selector(updateUi), userInfo: nil, repeats: false)
+    }
+    
+    @objc func updateUi(){
+                
+        questionLabel.text = quizBrain.getQuestionText()
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+        let percentageProgress = quizBrain.getProgress()
+        progressBar.progress = percentageProgress
+        scoreLabel.text = "Score:\(quizBrain.getScore())"
+        
+    }
+    
+}
+
